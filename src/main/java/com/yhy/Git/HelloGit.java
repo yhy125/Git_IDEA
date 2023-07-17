e276cdfba42fff74b7c543622f1d20205cb9f87c
YHY <hongyiyangyhy@163.com>
2023-07-17 21:58:10 +0800

    yhy first commit

---
+++ src/main/java/com/yhy/Git/HelloGit.java
@@ -0,0 +1,16 @@
+package com.yhy.Git;
+
+/**
+ * @Description:
+ * @ProjectName: Git_IDEA
+ * @Package: com.yhy.Git
+ * @Author: hongyiyangyhy@163.com
+ * @Date: 2023/7/17 21:54
+ * @Version: 1.0
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git ...");
+    }
+}
